fcc1b5f90b44194c9547ae04ac343c64a0e91c47
valerie <ooakhu@gmail.com>
2020-10-06 22:33:52 +0100

    updates

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,8 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
 urlpatterns = [
     path('', include('spiders.urls')),
     path('admin/', admin.site.urls),
